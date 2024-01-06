f12d35b62511d6c6014a002615d57f04e8480b1e
Uzo Nwokoro <uzo.tammy@gmail.com>
2024-01-06 17:20:17 -0400

    modified url of scush

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', MainView.as_view(), name='index'),
     path('aboutme/', AboutMe.as_view(), name='about-me'),
     path('resume-view/', ResumeView.as_view(), name='resume'),
-    path('portfolio/scush', PortfolioScushView.as_view(), name='portfolio-scush'),
+    path('portfolio-scush/', PortfolioScushView.as_view(), name='portfolio-scush'),
     path('portfolio-finuel/', PortfolioFinuelView.as_view(), name='portfolio-finuel'),
     path('texting-it', TextingView.as_view(), name='texting-it'),
     
