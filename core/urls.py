580207d105c28e71fd30cd0a890036a41b818309
Uzo Nwokoro <uzo.tammy@gmail.com>
2024-06-28 17:23:07 -0300

    improved design

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from .views import (MainView, AboutMe, ResumeView, PortfolioScushView, PortfolioFinuelView,
-                     TextingView, NumberToWordView, PortfolioRootView, DatabaseLearningView)
+                     TextingView, NumberToWordView, PortfolioRootView, DatabaseLearningView,
+                     AutomaticEmailView, NewHomeView, NewAboutMeView)
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
     path('aboutme/', AboutMe.as_view(), name='about-me'),
+    path('new-about-me', NewAboutMeView.as_view(), name='new-aboutme'),
     path('resume-view/', ResumeView.as_view(), name='resume'),
     path('portfolio-scush/', PortfolioScushView.as_view(), name='portfolio-scush'),
     path('portfolio-finuel/', PortfolioFinuelView.as_view(), name='portfolio-finuel'),
     path('texting-it/', TextingView.as_view(), name='texting-it'),
+    path('new-home/', NewHomeView.as_view(), name='new-home')
 
     
 ]
@@ -17,6 +20,5 @@ urlpatterns += [
     path('portfolio-root/', PortfolioRootView.as_view(), name='portfolio-root'),
     path('portfolio/miniproject/number-to-word/', NumberToWordView.as_view(), name='number-to-word'),
     path('portfolio/miniproject/database-learning/', DatabaseLearningView.as_view(), name='database-learning'),
-    
-    
+    path('portfolio/miniproject/auto-email/', AutomaticEmailView.as_view(), name='auto-email'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
 from typing import Any
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.views.generic.base import TemplateView
 
 from .forms import NumberInputForm
@@ -22,7 +29,7 @@ class PortfolioRootView(TemplateView):
     template_name = 'core/portfolio/root.html'
 
 class PortfolioScushView(TemplateView):
-    template_name = 'core/portfolio/scushproject.html'
+    template_name = 'core/portfolio/scush.html'
 
 
 class PortfolioFinuelView(TemplateView):
@@ -53,3 +60,12 @@ class NumberToWordView(TemplateView):
     
 class DatabaseLearningView(TemplateView):
     template_name = 'core/portfolio/tinyprojects/databaselearning.html'
+
+class AutomaticEmailView(TemplateView):
+    template_name = 'core/portfolio/tinyprojects/auto_email.html'
+
+class NewHomeView(TemplateView):
+    template_name = 'core/home.html'
+
+class NewAboutMeView(TemplateView):
+    template_name = 'core/new_about_me.html'
